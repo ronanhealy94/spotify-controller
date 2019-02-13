92a3d3deddb5f4cbf54ef0c3ce051da2dc8c39c6
Ronan Healy <ronanhealy94@gmail.com>
2019-02-13 20:57:21 +0000

    Initial commit
    Set up

--- src/App.js
+++ src/App.js
@@ -2,24 +2,57 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let defaultTextColor = '#fff';
+let defaultStyle = {
+    color: '#fff',
+
+};
+
+class Summary extends Component {
+  render () {
+    return (
+      <div style={{width: '40%', display: 'inline-block'}}>
+        <h2 style={{color: defaultTextColor}}>Number Text</h2>
+      </div>
+    );
+  }
+}
+
+class Filter extends Component {
+  render () {
+    return (
+      <div>
+        <img/>
+        <input type="text"/>
+      </div>
+    );
+  }
+}
+
+class Playlist extends Component {
+  render () {
+    return (
+      <div style={{...defaultStyle, display: 'inline-block', width: '25%'}}>
+        <img/>
+        <h3>Playlist Name</h3>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song31</li></ul>
+      </div>
+    );
+  }
+}
+
 class App extends Component {
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <h1>Title</h1>
+      <Summary/>
+      <Summary/>
+      <Filter/>
+      <Playlist/>
+      <Playlist/>
+      <Playlist/>
       </div>
     );
   }
