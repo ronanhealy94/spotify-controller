cfcab56712ffdc57c92574172f7b5e3a5b2d1f69
Ronan Healy <ronanhealy94@gmail.com>
2019-02-13 21:03:52 +0000

    CSS edits
    Cleaned up stuff

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-let defaultTextColor = '#fff';
 let defaultStyle = {
     color: '#fff',
 
@@ -12,7 +11,7 @@ class Summary extends Component {
   render () {
     return (
       <div style={{width: '40%', display: 'inline-block'}}>
-        <h2 style={{color: defaultTextColor}}>Number Text</h2>
+        <h2 style={defaultStyle}>Number Text</h2>
       </div>
     );
   }
@@ -42,11 +41,10 @@ class Playlist extends Component {
 }
 
 class App extends Component {
-
   render() {
     return (
       <div className="App">
-      <h1>Title</h1>
+      <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
       <Summary/>
       <Summary/>
       <Filter/>
