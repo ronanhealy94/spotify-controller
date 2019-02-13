4634b81a05a57bc6a06a4ecff12c0bffc2a9e903
Ronan Healy <ronanhealy94@gmail.com>
2019-02-13 22:08:32 +0000

    Fake Dat
    Entered fake data

--- src/App.js
+++ src/App.js
@@ -6,12 +6,41 @@ let defaultStyle = {
     color: '#fff',
 
 };
+let fakeServerData = {
+  user: {
+    name: 'David',
+    playlists: [
+      {
+        name: 'My favs',
+        songs: ['Beat It', 'Beat It', 'Beat It']
+      },
+      {
+        name: 'My favs',
+        songs: ['Beat It', 'Beat It', 'Beat It']
+      },
+      {
+        name: 'My favs',
+        songs: ['Beat It', 'Beat It', 'Beat It']
+      }
+    ]
+  }
+};
 
 class Summary extends Component {
   render () {
     return (
       <div style={{width: '40%', display: 'inline-block'}}>
-        <h2 style={defaultStyle}>Number Text</h2>
+        <h2 style={defaultStyle}>{this.props.playlists && this.props.playlists.length} Text</h2>
+      </div>
+    );
+  }
+}
+
+class Summary2 extends Component {
+  render () {
+    return (
+      <div style={{width: '40%', display: 'inline-block'}}>
+        <h2 style={defaultStyle}>{this.props.playlists && this.props.playlists.length} Text</h2>
       </div>
     );
   }
@@ -34,23 +63,43 @@ class Playlist extends Component {
       <div style={{...defaultStyle, display: 'inline-block', width: '25%'}}>
         <img/>
         <h3>Playlist Name</h3>
-        <ul><li>Song 1</li><li>Song 2</li><li>Song31</li></ul>
+        <ul><li>Song 1</li><li>Song 2</li><li>Song3</li></ul>
       </div>
     );
   }
 }
 
 class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {serverData: {}}
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+        this.setState({serverData: fakeServerData});
+  }, 1000);
+  }
   render() {
     return (
       <div className="App">
-      <h1 style={{...defaultStyle, 'font-size': '54px'}}>Title</h1>
-      <Summary/>
-      <Summary/>
-      <Filter/>
-      <Playlist/>
-      <Playlist/>
-      <Playlist/>
+        {this.state.serverData.user ?
+        <div>
+          <h1 style={{...defaultStyle, 'font-size': '54px'}}>
+            {this.state.serverData.user.name}'s playlists
+          </h1>
+
+          <Summary playlists={this.state.serverData.user.name}/>
+          <Summary2 playlists={this.state.serverData.user.name}/>
+      
+          <Filter/>
+
+          <Playlist/>
+          <Playlist/>
+          <Playlist/>
+        </div> : 'Loading...'
+        }
       </div>
     );
   }
